e6e4a39244d1753a8cad59d81991960049206760
Michał Kaczor <mk.kaczor24@gmail.com>
2025-12-14 12:18:01 +0100

    Added missing pages for legal routes

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+import { IconHammer } from "@tabler/icons-react";
+
+export default function AboutPage() {
+  return (
+    <div className="relative flex h-full flex-col items-center justify-center gap-4 px-4 py-32">
+      <h1 className="text-4xl text-neutral-50">Nothing to see here yet</h1>
+      <IconHammer className="h-16 w-16 text-neutral-50" />
+    </div>
+  );
+}
